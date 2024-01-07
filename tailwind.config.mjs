b61facd72f5564099273a71cc2ccf05ede227b89
Arturo Filastò <arturo@filasto.net>
2024-01-07 17:08:42 +0100

    Reimplement my homepage

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    colors: {
+      "light-yellow": "#FEEDC4",
+      "strong-red": "#E50D0E",
+      "medium-red": "#7D010A",
+      "dark-red": "#521611",
+      "off-black": "#1E0F0D",
+    },
+    extend: {},
+  },
+  plugins: [require("@tailwindcss/typography")],
+};
